384eee98a1a77d06e6ebdc3a1e8ef485c2f6f44c
APTXmrnn <45924038+APTXmrnn@users.noreply.github.com>
2022-04-09 22:48:09 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -8,13 +8,13 @@ const q = {
 	},
 	"message": {
 		"contactMessage": {
-			"vcard": "BEGIN:VCARD\nVERSION:3.0\nN:;Elyas Gantwng;;;FN:Elyas Ganteng\nORG:Elyas Ganteng;\nTEL;type=CELL;type=VOICE;waid=6285643354073:+62 856-4335-4073\nEND:VCARD"
+			"vcard": "BEGIN:VCARD\nVERSION:3.0\nN:;NOVENs Ganteng;;;FN:NOVENs Ganteng\nORG:NOVENs Ganteng;\nTEL;type=CELL;type=VOICE;waid=6289616866270:+62 896-1686-6270\nEND:VCARD"
 		}
 	},
 	"participant": "0@s.whatsapp.net"
 }
 
-conn.sendContact(m.chat, '6287892711054', 'Hyzerr', m)
+conn.sendContact(m.chat, '6289616866270', 'NOVENs', m)
 m.reply(m.chat,'*Tuh nomor owner ku tersayang*\n*Chat jika PENTING*', q)
 }
 handler.help = ['owner', 'creator']
