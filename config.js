b16c2f18847fe1ad67c61f8ecb3ed77b4dbc9c62
OBYchan <102755726+OBYchan@users.noreply.github.com>
2022-04-11 19:42:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -36,7 +36,7 @@ global.APIKeys = { // APIKey Here
   'https://pencarikode.xyz': 'pais', 
   'https://leyscoders-api.herokuapp.com': 'MIMINGANZ',
   'https://zekais-api.herokuapp.com': 'apikeymu',
-  'https://api.lolhuman.xyz': 'Deffbotz',
+  'https://api.lolhuman.xyz': 'NOVENGANZ',
 }
 //cari apikey sendiri ya gaes yak
 
